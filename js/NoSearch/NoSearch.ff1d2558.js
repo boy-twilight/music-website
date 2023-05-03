feffc21a84f498f8c34e86a92ce1cea614d3c9df
arrive-Lee <2429162154@qq.com>
2023-05-03 13:35:56 +0800

    project

---
+++ js/NoSearch/NoSearch.ff1d2558.js
@@ -0,0 +1,1 @@
+import{G as s,o as a,c as e,a as r,u as o}from"../@vue/@vue.69a1071d.js";import{_ as t}from"../../assets/index-a744d8a6.js";const c=""+new URL("../../assets/暂无搜索结果-08bd1716.svg",import.meta.url).href,m={class:"no-search"},d=["src"],i=t(s({__name:"NoSearch",setup:s=>(s,t)=>(a(),e("div",m,[r("img",{src:o(c),alt:""},null,8,d)]))}),[["__scopeId","data-v-e2d490c4"]]);export{i as N};
