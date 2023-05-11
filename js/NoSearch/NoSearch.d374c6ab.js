2fd5270fa8eea6a263199d17c6df8521f772767d
arrive-Lee <2429162154@qq.com>
2023-05-11 13:20:15 +0800

    add some function

--- js/NoSearch/NoSearch.d374c6ab.js
+++
@@ -1,1 +0,0 @@
-import{G as s,o as a,c as e,a as r,u as o}from"../@vue/@vue.7ad02ad1.js";import{_ as t}from"../../assets/index-96eff843.js";const c=""+new URL("../../assets/暂无搜索结果-08bd1716.svg",import.meta.url).href,m={class:"no-search"},d=["src"],i=t(s({__name:"NoSearch",setup:s=>(s,t)=>(a(),e("div",m,[r("img",{src:o(c),alt:""},null,8,d)]))}),[["__scopeId","data-v-e2d490c4"]]);export{i as N};
